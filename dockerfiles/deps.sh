3ea0c6e2f99821869fb5816f0405245414e43b07
Stefano <stefano.dafarra@gmail.com>
2022-07-21 18:13:15 +0200

    Added dockerfiles.

---
+++ dockerfiles/deps.sh
@@ -0,0 +1,29 @@
+apt update && \
+apt install -y -qq \
+make \
+git \
+wget \
+tar \
+patch \
+dos2unix \
+pkg-config \
+gfortran \
+liblapack-dev \
+libblas-dev \
+libopenblas-dev \
+libmetis-dev \
+build-essential \
+cmake \
+libeigen3-dev \
+libxml2-dev \
+libassimp-dev \
+libirrlicht-dev \
+libmatio-dev \
+ffmpeg \
+libavcodec-dev \
+libavformat-dev \
+libavdevice-dev \
+libavfilter-dev \
+libswscale-dev \
+libpostproc-dev \
+tmuxinator
